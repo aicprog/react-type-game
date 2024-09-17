1dbf61966be1123ddf18be5b0682a4f5f59b1faa
aicprog <aicprog@gmail.com>
2024-09-17 11:36:41 -0700

    Add react router with dummy pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Game from './pages/Game';
+import GameOver from './pages/GameOver';
+import Home from './pages/Home';
+
+//Routes for pages
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/game',
+		element: <Game />,
+	},
+	{
+		path: '/gameOver',
+		element: <GameOver />,
+	},
+	{
+		path: '/home',
+		element: <Home />,
+	},
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
---
+++ src/pages/HighScores.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HighScores = () => {
+  return (
+    <div>HighScores</div>
+  )
+}
+
+export default HighScores
